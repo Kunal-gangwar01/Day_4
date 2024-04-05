5b946272662c9f596ec871d2b65d68b16b5d0043
Kunal <gangwarkunal327@gmail.com>
2024-04-05 10:33:21 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// let a= new String("hello");
+// let b=("hello");
+// if(a==b){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// const obj={
+//     firstName : "Kunal",
+//     lastName:"Gangwar",
+//     age:23,
+// }
+// console.log(obj);
+
+
+// const obj={
+// name:"kunal",
+// lastName:"Gangwar",
+// }
+// obj.age=20;
+// console.log(obj);
+
+// let obj={
+// name:"kunal",
+// lastName:"Gangwar",
+// }
+
+// let obj1={
+// name:"kunal",
+// lastName:"Gangwar",
+// }
+// console.log(obj);
+
+// let arr = ['a','b','c','d'];
+// arr = [1,2,3,4];
+// arr[10]=20;
+// arr.push(10);  //push the elements in the array--
+// console.log(arr);
+// console.log(typeof arr);
+
+let obj={
+    name:"kunal",
+    lastName:"Gangwar",
+    }
+    console.log(Array.isArray(obj));
+const arr = [1,2,3,4];
+arr[10]=20;
+// arr.push(10);  //push the elements in the array--
+// console.log(arr);
+console.log(Array.isArray(arr));
+
+// console.log(typeof arr);
