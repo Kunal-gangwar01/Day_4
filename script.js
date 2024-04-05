6ca6bd746145bb47ccc22c012a5d5acf574b2d14
Kunal <gangwarkunal327@gmail.com>
2024-04-05 12:28:36 +0530

    message

--- script.js
+++ script.js
@@ -40,15 +40,64 @@
 // console.log(arr);
 // console.log(typeof arr);
 
-let obj={
-    name:"kunal",
-    lastName:"Gangwar",
-    }
-    console.log(Array.isArray(obj));
-const arr = [1,2,3,4];
-arr[10]=20;
+// let obj={
+//     name:"kunal",
+//     lastName:"Gangwar",
+//     }
+//     console.log(Array.isArray(obj));
+// const arr = [1,2,3,4];
+// arr[10]=20;
 // arr.push(10);  //push the elements in the array--
 // console.log(arr);
-console.log(Array.isArray(arr));
+// console.log(Array.isArray(arr));
 
 // console.log(typeof arr);
+
+// const arr= ['name', 2,7];
+// const obj = {
+// fullName :"ajay",
+// age:21,
+// };
+// for( let i of arr){
+//     console.log(i); //'of' is for to print array elements
+// }
+
+// for( let i in arr){ //'in' is for to print index of array
+//     console.log(i);
+// }
+// for( let i in obj){ //'in' is for to print obj var
+//     console.log(i);
+// }
+
+// console.log(window);
+// console.log(window.innerHeight);
+// console.log(innerWidth);
+// console.log(window.document);
+// console.dir(window.document);
+
+// const res = document.getElementById('ht1');
+// console.log(res);
+
+// const res = document.querySelectorAll('h1');
+// console.log(res);
+
+// const res = document.getElementById('ht1');
+// // res['innerText'] = "Hello World";
+// const res = document.getElementByTagName('p');
+// res[0].tagName="h1";
+
+// console.log(res);
+
+// const ne = document.createElement("h3");
+// ne.innerText = "Dynamic Text";
+// // console.log(ne);
+// document.body.appendChild(ne);
+
+
+const ne = document.createElement("p");
+ne.innerText = "Dynamic Text";
+const secondDiv = document.getElementsByTagName("p");
+secondDiv[0].appendChild(ne);
+
+
+
