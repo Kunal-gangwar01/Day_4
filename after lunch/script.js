f87762ed4644e75a5a39d69fa1a474b40dfee0cb
Kunal <gangwarkunal327@gmail.com>
2024-04-08 12:28:03 +0530

    day 4

--- after lunch/script.js
+++ after lunch/script.js
@@ -22,12 +22,54 @@
 //  {
 //     console.log(e.target.value);
 // }
-const submitForm = (e)=>{
-    e.preventDefault();
-    for(let i=0;i<6;i++){
-        console.log(e.target[i].value);
 
+
+// const submitForm = (e)=>{
+//     e.preventDefault();
+
+//     for(let i=0;i<5;i++){
+//         console.log(e.target[i].value);
+//     }
+//     console.log(e);
+// }
+
+
+function submitForm(e) {
+    e.preventDefault();
+    const t = e.target;
+    for (let i = 0; i < t.length; i++) {
+        const ty = t[i].type;
+        const vl = t[i].value;
+        console.log(ty, vl);
+        if (ty == 'checkbox') {
+            console.log(t[i].checked)
+        }
     }
-    console.log(e);
 }
+
+function submitForm(e) {
+    e.preventDefault();
+    const t = e.target;
+    const res = {
+        hobbies: [],
+    };
+    for (let i = 0; i < t.length; i++) {
+        const ty = t[i].type;
+        if (ty != 'submit') {
+            const vl = t[i].value;
+            const nm = t[i].name;
+
+            if (ty == 'checkbox' && t[i].checked) {
+                res.hobbies.push(vl);
+            }
+            if (ty != 'checked') {
+                res[nm] = vl;
+            }
+        }
+    }
+    console.log(res);
+}
+
+
+
 
